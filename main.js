ccfa9922c22f99e940a1321f18f22550e3b6a9df
peziiim <eduardoorx@gmail.com>
2026-07-22 20:28:53 -0300

    feat: app window

---
+++ main.js
@@ -0,0 +1,27 @@
+require('electron-reload')(__dirname) 
+
+const { app, BrowserWindow, ipcMain } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 320,
+    height: 420,
+    resizable: false,
+    maximizable: false,
+    fullscreenable: false,
+    center: true,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('index.html');
+  ipcMain.on('window:minimize', () => win.minimize());
+  ipcMain.on('window:close', () => win.close());
+};
+
+app.whenReady().then(createWindow);
